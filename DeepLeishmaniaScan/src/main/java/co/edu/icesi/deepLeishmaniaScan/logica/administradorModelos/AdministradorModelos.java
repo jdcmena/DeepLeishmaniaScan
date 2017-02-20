9b999399e29fe1f292efb19f7a0b5dd30d4f304e
jdcm <jdcm@jdcm-VirtualBox>
2017-02-20 16:27:09 -0500

    read & write json successfull, load model successfull, aparent model admin finished

--- DeepLeishmaniaScan/src/main/java/co/edu/icesi/deepLeishmaniaScan/logica/administradorModelos/AdministradorModelos.java
+++ DeepLeishmaniaScan/src/main/java/co/edu/icesi/deepLeishmaniaScan/logica/administradorModelos/AdministradorModelos.java
@@ -1,22 +1,25 @@
 package co.edu.icesi.deepLeishmaniaScan.logica.administradorModelos;
 
-import com.google.gson.Gson;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+
 public class AdministradorModelos implements IAdministradorModelos {
 	private static final char OS = File.separatorChar;
-	private static final String MODELS_DIRECTORY = "."+OS+"src"+OS+"main"+OS+"resources"+OS+"models"+OS+"";
-	private static final String MODELS_LIST = "."+OS+"src"+OS+"main"+OS+"resources"+OS+"modelsList.txt";
+	private static final String MODELS_DIRECTORY = "." + OS + "src" + OS + "main" + OS + "resources" + OS + "models"
+			+ OS + "";
+	private static final String MODELS_LIST = "." + OS + "src" + OS + "main" + OS + "resources" + OS + "modelsList.txt";
 	private static final String NEW_MODEL_TEMPLATE = MODELS_DIRECTORY;
-	
-	private ArrayList<Modelo> listaModelos; 
-	
+
+	private ArrayList<Modelo> listaModelos;
 
 	public AdministradorModelos() {
 		try {
@@ -35,41 +38,64 @@ public class AdministradorModelos implements IAdministradorModelos {
 		// TODO - implement AdministradorModelos.cargarModelo
 		throw new UnsupportedOperationException();
 	}
+
 	/**
 	 * Cuando se va a guardar un modelo NUEVO
 	 */
 	@Override
-	public void guardarModelo(Modelo model, String[] runConfigParams) throws Exception { //TODO Ready to Test
+	public void guardarModelo(Modelo model, String[] runConfigParams) throws Exception { // TODO
+																							// Ready
+																							// to
+																							// Test
 		final Gson gson = new Gson();
-		String folderPath = NEW_MODEL_TEMPLATE+model.getID()+OS;
-		String jsonRunConfigPath = NEW_MODEL_TEMPLATE+model.getID()+OS+"runconfig.json";
-		
+		String folderPath = NEW_MODEL_TEMPLATE + model.getID() + OS;
+		String jsonRunConfigPath = NEW_MODEL_TEMPLATE + model.getID() + OS + "runconfig.json";
+
 		File modelJson = new File(folderPath);
-		//modelJson.mkdirs();
+		// modelJson.mkdirs();
 		modelJson.mkdir();
 		modelJson.createNewFile();
-		
-		modelJson = new File(folderPath+model.getID()+".json");
+
+		modelJson = new File(folderPath + model.getID() + ".json");
 		modelJson.getParentFile().mkdirs();
 		modelJson.createNewFile();
-		
-		File modelJsonRunConfig = new File(NEW_MODEL_TEMPLATE+model.getID()+OS+"runconfig.json");
+
+		File modelJsonRunConfig = new File(NEW_MODEL_TEMPLATE + model.getID() + OS + "runconfig.json");
 		modelJsonRunConfig.getParentFile().mkdirs();
 		modelJsonRunConfig.createNewFile();
 		
+		FileWriter fw1 = new FileWriter(folderPath + model.getID() + ".json");
+		FileWriter fw2 = new FileWriter(jsonRunConfigPath);
 		
-		gson.toJson(model, new FileWriter(folderPath+model.getID()+".json"));
-		gson.toJson(runConfigParams, new FileWriter(jsonRunConfigPath));
+		String j1 = gson.toJson(model);
+		String j2 = gson.toJson(runConfigParams);
+		fw1.write(j1);
+		fw2.write(j2);
+		fw1.flush();
+		fw2.flush();
+		//gson.toJson(j1, fw1);
+		//gson.toJson(j2, fw2);
+		File modelList = new File(MODELS_LIST);
+		FileWriter fw = new FileWriter(modelList);
+
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.append(model.getID()+"");
+		fw1.close();
+		fw2.close();
+		bw.close();
+
 	}
 
 	/**
 	 * Buscar un modelo ya creado
+	 * 
 	 * @param id
 	 */
 	@Override
-	public Modelo getModeloPorId(String id) throws IOException{ //TODO Ready to test
+	public Modelo getModeloPorId(String id) throws IOException { // TODO Ready
+																	// to test
 		final Gson gson = new Gson();
-		Modelo prop = gson.fromJson(new FileReader(NEW_MODEL_TEMPLATE+id+OS+id+".json"), Modelo.class);
+		Modelo prop = gson.fromJson(new FileReader(NEW_MODEL_TEMPLATE + id + OS + id + ".json"), Modelo.class);
 		return prop;
 	}
 
@@ -84,7 +110,8 @@ public class AdministradorModelos implements IAdministradorModelos {
 	 * @param nesterov
 	 */
 	@Override
-	public void setParametrosModelo(Modelo model, int nEpoch, int nImgPerEpoch, double lR, double mR, double dLr, boolean nesterov) {
+	public void setParametrosModelo(Modelo model, int nEpoch, int nImgPerEpoch, double lR, double mR, double dLr,
+			boolean nesterov) {
 		// TODO - implement AdministradorModelos.setParametrosModelo
 		throw new UnsupportedOperationException();
 	}
@@ -93,37 +120,37 @@ public class AdministradorModelos implements IAdministradorModelos {
 	public List<Modelo> getListaModelos() {
 		return listaModelos;
 	}
-	
+
 	/**
 	 * 
-	 * @param runConfigParams has:
-	 * [0]: epoch number: int
-	 * [1]: images per epoch: int
-	 * [2]: learning rate : double
-	 * [3]: decay rate of learning rate : double
-	 * [4]: nesterov technique for optimization: boolean
-	 * [5]: name
+	 * @param runConfigParams
+	 *            has: [0]: epoch number: int [1]: images per epoch: int [2]:
+	 *            learning rate : double [3]: decay rate of learning rate :
+	 *            double [4]: nesterov technique for optimization: boolean [5]:
+	 *            name
 	 */
 	@Override
-	public void crearModelo(String nombre, String[] runConfigParams) throws Exception{ //TODO
+	public void crearModelo(String nombre, String[] runConfigParams) throws Exception { // TODO
 		Modelo modelo = new Modelo(nombre);
-		if(!modelExist(modelo.getID()+"")){
-			createModelFolder(modelo.getID()+"");
+		if (!modelExist(modelo.getID() + "")) {
+			createModelFolder(modelo.getID() + "");
 		}
 		guardarModelo(modelo, runConfigParams);
-		
+
 	}
+
 	/**
 	 * verifica si el nombre del modelo pasado por parametro ya existe
+	 * 
 	 * @param modelId
 	 * @return true o false
 	 * @throws IOException
 	 */
-	private boolean modelExist(String modelId) throws IOException{
+	private boolean modelExist(String modelId) throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(new File(MODELS_LIST)));
 		String line = "";
-		while((line = reader.readLine())!=null){
-			if(line.equals(modelId)){
+		while ((line = reader.readLine()) != null) {
+			if (line.equals(modelId)) {
 				reader.close();
 				return true;
 			}
@@ -131,55 +158,58 @@ public class AdministradorModelos implements IAdministradorModelos {
 		reader.close();
 		return false;
 	}
-	
+
 	///////////////
-	
+
 	//////////////
-	
+
 	/**
-	 * crea el directorio raiz de los modelos y crea el archivo con los nombres de modelos si no existe, y los lee
+	 * crea el directorio raiz de los modelos y crea el archivo con los nombres
+	 * de modelos si no existe, y los lee
+	 * 
 	 * @throws IOException
 	 */
-	private void createRootModelsFolder() throws IOException{
+	private void createRootModelsFolder() throws IOException {
 		File mainFolder = new File(MODELS_DIRECTORY);
-		if(!mainFolder.exists()){
+		if (!mainFolder.exists()) {
 			mainFolder.mkdirs();
 			mainFolder.createNewFile();
 			System.out.println(mainFolder.getAbsolutePath());
 		}
 		File modelList = new File(MODELS_LIST);
-		if(!modelList.exists()){
-		modelList.getParentFile().mkdirs();
-		modelList.createNewFile();
+		if (!modelList.exists()) {
+			modelList.getParentFile().mkdirs();
+			modelList.createNewFile();
 		}
 		System.out.println(modelList.getAbsolutePath());
 		loadSavedModels(modelList);
 	}
+
 	/**
-	 * Lee los nombres de los modelos y los convierte en clase Modelo
-	 * Models must be saved as a string like this: [modelName].json 
+	 * Lee los nombres de los modelos y los convierte en clase Modelo Models
+	 * must be saved as a string like this: [modelName].json
+	 * 
 	 * @param modelsTxt
 	 * @throws IOException
 	 */
-	private void loadSavedModels(File modelsTxt) throws IOException{ //TODO Ready to test
+	private void loadSavedModels(File modelsTxt) throws IOException { // TODO
+		
 		listaModelos = new ArrayList<>();
 		BufferedReader reader = new BufferedReader(new FileReader(new File(MODELS_LIST)));
 		String line = "";
-		while((line = reader.readLine())!=null){
+		while ((line = reader.readLine()) != null) {
 			Modelo modelo = getModeloPorId(line);
 			listaModelos.add(modelo);
 		}
 		reader.close();
 	}
-	
-	private void createModelFolder(String nombre) throws IOException{
-		File mainFolder = new File(NEW_MODEL_TEMPLATE+nombre);
-		if(!mainFolder.exists()){
-			mainFolder.getParentFile().mkdir();
+
+	private void createModelFolder(String nombre) throws IOException {
+		File mainFolder = new File(NEW_MODEL_TEMPLATE + nombre);
+		if (!mainFolder.exists()) {
+			mainFolder.mkdir();
 			mainFolder.createNewFile();
 		}
 	}
-
-
 
 }
--- DeepLeishmaniaScan/src/main/java/co/edu/icesi/deepLeishmaniaScan/vista/backend/PanelModelos.java
+++ DeepLeishmaniaScan/src/main/java/co/edu/icesi/deepLeishmaniaScan/vista/backend/PanelModelos.java
@@ -22,45 +22,47 @@ import javax.tools.DiagnosticListener;
 import co.edu.icesi.deepLeishmaniaScan.logica.administradorModelos.Modelo;
 
 public class PanelModelos extends JPanel implements ActionListener {
-	
+
 	private BackendView principal;
-	
-	private JList listaModelos;
-	
+
+	private JList<Modelo> listaModelos;
+
 	private Modelo modeloSeleccionado;
 
 	private JButton btnEditarHiperparametros;
 
 	private JButton btnNuevoModelo;
-	
+
 	private DefaultListModel<Modelo> dlm;
-	
-	public PanelModelos(BackendView ventana){
+
+	public PanelModelos(BackendView ventana) {
 		principal = ventana;
 		setLayout(new BorderLayout());
 		fillDLF();
 		listaModelos = new JList<>(dlm);
 		listaModelos.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		listaModelos.addListSelectionListener(new ListSelectionListener() {
-			
+
 			@Override
 			public void valueChanged(ListSelectionEvent e) {
-				modeloSeleccionado = (Modelo)e.getSource();
+
+				JList<Modelo> source = (JList) e.getSource();
+				modeloSeleccionado = (Modelo) source.getSelectedValue();
 				habilitarHiperP();
 			}
 		});
 		this.add(listaModelos, BorderLayout.CENTER);
-		
-		
+
 		JPanel panel_1 = new JPanel();
 		this.add(panel_1, BorderLayout.SOUTH);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel_1.rowHeights = new int[]{0, 0};
-		gbl_panel_1.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel_1.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gbl_panel_1.rowHeights = new int[] { 0, 0 };
+		gbl_panel_1.columnWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
+		gbl_panel_1.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
 		panel_1.setLayout(gbl_panel_1);
-		
+
 		btnEditarHiperparametros = new JButton("Editar Hiperparametros");
 		btnEditarHiperparametros.setActionCommand("EH");
 		btnEditarHiperparametros.addActionListener(this);
@@ -71,7 +73,7 @@ public class PanelModelos extends JPanel implements ActionListener {
 		gbc_btnNewButton.gridx = 0;
 		gbc_btnNewButton.gridy = 0;
 		panel_1.add(btnEditarHiperparametros, gbc_btnNewButton);
-		
+
 		btnNuevoModelo = new JButton("Crear nuevo modelo");
 		btnNuevoModelo.addActionListener(this);
 		btnNuevoModelo.setActionCommand("NM");
@@ -82,37 +84,38 @@ public class PanelModelos extends JPanel implements ActionListener {
 		gbc_btnNewButton_1.gridy = 0;
 		panel_1.add(btnNuevoModelo, gbc_btnNewButton_1);
 		deshabilitarHiperP();
-		
+
 	}
 
-	public Modelo getModeloSeleccionado(){
+	public Modelo getModeloSeleccionado() {
 		return modeloSeleccionado;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String com = e.getActionCommand();
-		switch(com){
+		switch (com) {
 		case "NM":
 			DialogCreateModel dlg = new DialogCreateModel(principal);
 			dlg.setVisible(true);
 			break;
-			
+
 		case "EH":
 			JOptionPane.showMessageDialog(this, "edit hiperparameters");
 			break;
-			
+
 		}
 	}
-	
-	public void habilitarHiperP(){
+
+	public void habilitarHiperP() {
 		btnEditarHiperparametros.setEnabled(true);
 	}
-	public void deshabilitarHiperP(){
+
+	public void deshabilitarHiperP() {
 		btnEditarHiperparametros.setEnabled(false);
 	}
-	
-	private void fillDLF(){
+
+	private void fillDLF() {
 		dlm = new DefaultListModel<>();
 		for (Modelo modelo : principal.getListaModelos()) {
 			dlm.addElement(modelo);
